da9117c9529f171e76de7c813b1d9a854cd9af8f
bogdan <rusubogdan93@gmail.com>
2015-10-20 21:57:45 +0300

    wrong version

--- matrixMultiplication.c
+++ matrixMultiplication.c
@@ -1,8 +1,11 @@
 #include <mpi.h>
 #include <cstdio>
 #include <cstdlib>
+#include <unistd.h>
 
 using namespace std;
+
+int c[100][100];
 
 // give m n p from command line
 int main(int argc, char** argv) {
@@ -15,7 +18,7 @@ int main(int argc, char** argv) {
     p = atoi(argv[3]);
 
     int a[m][n], b[n][p]; // col.a = row.b for multiplication
-	int c[m][p]; // holding the result
+	// int c[m][p]; // holding the result
 
     MPI_Init(&argc, &argv);
 
@@ -26,8 +29,6 @@ int main(int argc, char** argv) {
 
 	int tag = 201; // for mpi send
 	int fragments = 0;
-
-	
 
 	int aux[m][n];
 
@@ -65,6 +66,9 @@ int main(int argc, char** argv) {
 		// send size of aux matrix to all nodes
 		MPI_Bcast(&maxSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
+		MPI_Request request;
+    	MPI_Status status;
+		
 		// exclude the master		
 		for (int i = 1; i < worldSize && worldRank * (maxSize-1) + i <= m; i++) {
 			int z = 0;
@@ -75,69 +79,77 @@ int main(int argc, char** argv) {
 				z++;
 			}
 
-			MPI_Send(&aux[0][0], maxSize * n, MPI_INT, i, tag, MPI_COMM_WORLD);
-	
+			MPI_Isend(&aux[0][0], maxSize * n, MPI_INT, i, tag, MPI_COMM_WORLD, &request);
+			MPI_Wait (&request, &status);
+			//MPI_Send(&z, 1, MPI_INT, i, tag, MPI_COMM_WORLD);
+			//sleep(1);
 		
+		}
+
+		sleep(5);
+		printf("C:______________ \n");
+		for (int i = 0; i < m; i++) {
+		
+			for (int j = 0; j < n; j++) {
+				printf("%d ", c[i][j]);
+			}
+			printf("\n");
 		}
 		
 			
 	} else {
+		int m_size;
+		MPI_Request request;
+    	MPI_Status status;
+
 		MPI_Bcast(&b[0][0], n * p, MPI_INT, 0, MPI_COMM_WORLD);
 		MPI_Bcast(&maxSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
 				
-		MPI_Recv(&aux[0][0], maxSize * n, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		
-		 int y = 0;
-printf("Node %d of %d \n", worldRank, worldSize);
+		MPI_Irecv(&aux[0][0], maxSize * n, MPI_INT, 0, tag, MPI_COMM_WORLD, &request);
+		//MPI_Recv(&m_size, 1, MPI_INT, 0, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Wait (&request, &status);
+		int y = 0;
+		printf("Node %d of %d \n", worldRank, worldSize);
 		for (int i = 0; i < maxSize && worldRank * (maxSize-1) + i <= m; i++) {
 
-			for(int g = 0; g < n; g++) {
-				printf("%d ", aux[i][g]);		
-			}
-			printf("\n");
-			// multiply aux and b
+		    // multiply aux and b
 			int z = 0; int s = 0;
 			int j = 0;			
 			for (j = 0; j < n ; j++) {
+				s = 0;
 				for (int z = 0; z < n ; z++) {
-s += aux[i][z] * b[z][maxSize*(worldRank-1)+j];					
-//printf("%d(%d,%d) * %d(%d,%d) = ", aux[i][z],i,z, b[z][maxSize*(worldRank-1)+j], z, maxSize*(worldRank-1)+j);
-			  		
-					//printf("%d ", aux[i][j]);
-				}
-				printf("%d ", s);
-				c[maxSize*(worldRank-1)+i][maxSize*(worldRank-1)+j] = s;
+					s += aux[i][z] * b[z][j];					
+ 			}
+				printf("c[%d][%d]=%d ",(worldRank-1)*maxSize + i,j,s);
+				c[(worldRank-1)*maxSize + i][j] = s;
+
+				
 			}
-			printf("\n");
-	
-
+			sleep(1);
+			printf("\n");printf("\n");printf("\n");
+			for (int i = 0; i < m; i++) {
+		
+				for (int j = 0; j < n; j++) {
+					printf("%d ", c[i][j]);
+				}
+				printf("\n");
+			}	
 		}
-		printf("\n"); 
 	}
-
-
- 	MPI_Finalize();          
 	
-	if (worldRank == 0) {	
+	// if (worldRank == 0) {
+	// 	sleep(5);
+	// 	printf("C:______________ \n");
+	// 	for (int i = 0; i < m; i++) {
+		
+	// 		for (int j = 0; j < n; j++) {
+	// 			printf("%d ", c[i][j]);
+	// 		}
+	// 		printf("\n");
+	// 	}
+	// }
 
-	for (int i = 0; i < m; i++) {
-		printf("%d -> ", i);
-		for (int j = 0; j < n; j++) {
-			printf("%d ", a[i][j]);
-		}
-		printf("     ");
-		
-		for (int j = 0; j < n; j++) {
-			printf("%d ", b[i][j]);
-		}
-		printf("     ");
-		
-		for (int j = 0; j < n; j++) {
-			printf("%d ", c[i][j]);
-		}
-		printf("\n");
-	}	
-}
-	
+
+	MPI_Finalize();	
     return 0;
 }
